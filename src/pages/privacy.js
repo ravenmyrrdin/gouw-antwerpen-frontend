63bc49e4b94b71cbcfdc519c6b6e4a77e903890a
Raven Van Hove <raven.vanhove@student.ap.be>
2023-03-18 11:03:15 +0100

    update privacy

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -6,11 +6,13 @@ import '../styles.css';
 const Privacy = ({ data }) => {
     const { wpPage } = data;
     const { title, content } = wpPage;
+    const url = typeof window !== 'undefined' ? window.location.href : '';
+    const newContent = content.replace("+++url+++", "<a href='" + url + "'>" + url + "</a>")
     return (
         <Layout>
             <div className="privacyPagBox">
                 <h1>{title}</h1>
-                <div dangerouslySetInnerHTML={{ __html: content }} />
+                <div dangerouslySetInnerHTML={{ __html: newContent }} />
             </div>
         </Layout>
     );
